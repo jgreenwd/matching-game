1f20380c57782757ba76a3ae8aafce94b662f2ec
Jeremy Greenwood <jgreenwd@hotmail.com>
2020-03-14 16:31:10 -0500

    Updates

---
+++ precache-manifest.98501b1a338026ba2e7f928605ca6710.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98efd5779282abc275c06786c5a4ad70",
+    "url": "/matching-game/index.html"
+  },
+  {
+    "revision": "8cf9ce25b4ade91e5ae1",
+    "url": "/matching-game/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "300fccde5d18c30c061b",
+    "url": "/matching-game/static/js/2.1e13a2de.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/matching-game/static/js/2.1e13a2de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cf9ce25b4ade91e5ae1",
+    "url": "/matching-game/static/js/main.0a4c8227.chunk.js"
+  },
+  {
+    "revision": "4d440de50313156e0138",
+    "url": "/matching-game/static/js/runtime-main.7837c5cc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/matching-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
